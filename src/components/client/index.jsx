2aa5425ea7b3a27123caf93a57de0c4adad5a579
xasano5o <xasansenior@gmail.com>
2024-02-04 19:13:03 +0500

    update

--- src/components/client/index.jsx
+++ src/components/client/index.jsx
@@ -15,45 +15,46 @@ export default function HomeIndex() {
         <>
             <div className=" fixed inset-0 z-50 flex items-center justify-center overflow-x-auto">
 
-
                 <div className=" border rounded-lg shadow-lg relative flex flex-col w-full h-full bg-white outline-none focus:outline-none">
-   
                     <h1 className="px-4 ">Bizning xizmatlar</h1>
-                        <Carousel className="w-full" autoPlay={true} infiniteLoop={true} showThumbs={false} showStatus={false} showArrows={true}>
-                            {datacarusel?.map((item, index) => (
-                                <div key={index + 1} className="  carousel-item active w-full" data-bs-interval="10000">
-                                    <img
-                                        src={item?.image}
-                                        className="object-cover w-full h-[400px] "
-                                        alt="banner_image"
-                                    />
-                                </div>
-                            ))}
-                        </Carousel>
+                    <Carousel className="w-full" autoPlay={true} infiniteLoop={true} showThumbs={false} showStatus={false} showArrows={true}>
+                        {datacarusel?.map((item, index) => (
+                            <div key={index + 1} className="  carousel-item active w-full" data-bs-interval="10000">
+                                <img
+                                    src={item?.image}
+                                    className="object-cover w-full h-[400px] "
+                                    alt="banner_image"
+                                />
+                            </div>
+                        ))}
+                    </Carousel>
                     <div className="p-6 bg-white flex-auto">
-                        <div className="flex gap-2">
-                            <h1 className="text-2xl font-bold mb-4">Turkumlar</h1>
-                  <NavLink className={'texts'} to={'/home'}>
-                  <h1 className=" text-2xl font-bold mb-4 cursor-pointer">Umumiy</h1>
-                  </NavLink>
+                    
+                    <NavLink className={'texts'} to={'/home'}>
+                        <div className="flex gap-2   items-center justify-center border rounded transition duration-300 no-underline hover:bg-gray-100">
+                            <h1 className="text-2xl font-bold mb-4"> Barcha Mahsulotlarni</h1>
+                          
+                          <br /> 
+                                <h1 className=" text-2xl font-bold mb-4 cursor-pointer">Umumiy Ko'rish</h1>
                         </div>
-
+                            </NavLink>
+  <br />
                         <div className="grid grid-cols-2 gap-4">
-  {data?.map((item) => (
-    <Link
-      className="flex items-center gap-2 p-2 border rounded transition duration-300 no-underline hover:bg-gray-100"
-      to={`/categories/${item.id}`}
-      onClick={(e) => {
-        e.preventDefault();
-        navigate(`/categories/${item.id}`);
-      }}
-      key={item.id}
-    >
-      <img src={item?.image} alt="" className="w-12 h-12 rounded-full" />
-      <p className="text-gray-800 text-sm sm:text-base md:text-lg">{item?.title}</p>
-    </Link>
-  ))}
-</div>
+                            {data?.map((item) => (
+                                <Link
+                                    className="flex items-center gap-2 p-2 border rounded transition duration-300 no-underline hover:bg-gray-100"
+                                    to={`/categories/${item.id}`}
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        navigate(`/categories/${item.id}`);
+                                    }}
+                                    key={item.id}
+                                >
+                                    <img src={item?.image} alt="" className="w-12 h-12 rounded-full" />
+                                    <p className="text-gray-800 text-sm sm:text-base md:text-lg">{item?.title}</p>
+                                </Link>
+                            ))}
+                        </div>
                         <br />
                         <br />
 
